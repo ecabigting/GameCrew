277f9c335776f153d5e6b33ab6b0728c4d93c2b6
ecabigting <ericcabigting@outlook.com>
2023-01-14 00:24:11 +0400

    - configure app settings - little clean up whitespaces - add all dependencies to Program.cs

--- src/gamecrew/Helpers/SessionExtensions.cs
+++ src/gamecrew/Helpers/SessionExtensions.cs
@@ -8,14 +8,12 @@ namespace gamecrew.Helpers
     {
         public static void SetObjectAsJson(this ISession session, string key, object value)
         {
-
             session.SetString(key, JsonConvert.SerializeObject(value));
         }
 
         public static T GetObjectFromJson<T>(this ISession session, string key)
         {
             var value = session.GetString(key);
-
             return value == null ? default(T) : JsonConvert.DeserializeObject<T>(value);
         }
 
--- src/gamecrew/Program.cs
+++ src/gamecrew/Program.cs
@@ -1,13 +1,57 @@
+using System;
+using gamecrew.Helpers;
+using gamecrew.Services;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);                    
 
-// Add services to the container.
+// Interesting Note I found from dotnet 7 docs:
+// WebApplication.CreateBuilder initializes a 
+// new instance of the WebApplicationBuilder class 
+// with preconfigured defaults. The initialized WebApplicationBuilder 
+// (builder) provides default configuration and calls AddUserSecrets when the EnvironmentName is Development
+// https://learn.microsoft.com/en-us/aspnet/core/security/app-secrets?view=aspnetcore-7.0&tabs=windows#register-the-user-secrets-configuration-source
+
+#region AppSettings
+AppSettings settings = new AppSettings{
+    ConnectionString = builder.Configuration["PlayerGroupsDatabaseSettings:ConnectionString"],
+    DatabaseName = builder.Configuration["PlayerGroupsDatabaseSettings:DatabaseName"],
+    Key = builder.Configuration["Tokens:Key"],
+    Exp = Convert.ToInt32(builder.Configuration["Tokens:Exp"]),
+};
+builder.Services.AddSingleton(settings);
+#endregion AppSettings
+
+#region MongoDB Collection Services
+builder.Services.AddScoped<IPlayerService, PlayerService>();
+builder.Services.AddScoped<IGroupServices, GroupService>();
+#endregion MongoDB Collection Services
+
+#region Context Accesor
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+#endregion Context Accesor
+
+#region Sessions
+builder.Services.AddSession(options =>
+            {
+                options.Cookie.Name = ".gamecrew.sessh";
+                options.IdleTimeout = TimeSpan.FromDays(15);
+                options.Cookie.IsEssential = true;
+                options.Cookie.MaxAge = TimeSpan.FromDays(15);
+            });
+#endregion Sessions
+
 builder.Services.AddControllersWithViews();
 
+
 var app = builder.Build();
+
+
+app.UseSession();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -19,11 +63,8 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
