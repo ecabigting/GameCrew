286988229940dc1a652ba920bf2db7809500a0df
ecabigting <ericcabigting@outlook.com>
2023-01-14 20:43:53 +0400

    added the encode image function in js

--- src/gamecrew/wwwroot/js/site.js
+++ src/gamecrew/wwwroot/js/site.js
@@ -1,4 +1,19 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿function encodeImgtoBase64(element) {
+    var file = element.files[0];
+    if (typeof (file) != "undefined") {
+        var size = parseFloat(file.size / 1000).toFixed(2);
+        if (size > 100) {
+            alert('Please select image size less than 100 kb');
+        } else {
+            var reader = new FileReader();
+            reader.onloadend = function () {
+                $("#convertImg").attr("href", reader.result);
+                $("#Image").val(reader.result.toString());
+                $("#ImagePrev").attr("src", reader.result);
+            }
+            reader.readAsDataURL(file);
+        }
+    } else {
+        alert("This browser does not support HTML5.");
+    }
+}
